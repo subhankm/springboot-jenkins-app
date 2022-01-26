878375d045f2a7b8e9b9b55919e4d7777a9992e9
subhankm <mabusubhan.k@gmail.com>
2022-01-27 01:15:41 +0530

    4th commit

--- src/main/java/com/subhan/springboot/app/SpringbootJenkinsAppApplication.java
+++ src/main/java/com/subhan/springboot/app/SpringbootJenkinsAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootJenkinsAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootJenkinsAppApplication.class, args);
 	}
 
