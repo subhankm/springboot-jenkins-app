9ed0048b5e93657c4774410c09a9ac1077f13863
subhankm <96390862+subhankm@users.noreply.github.com>
2022-01-29 06:27:13 +0530

    Update SpringbootJenkinsAppApplication.java

--- src/main/java/com/subhan/springboot/app/SpringbootJenkinsAppApplication.java
+++ src/main/java/com/subhan/springboot/app/SpringbootJenkinsAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootJenkinsAppApplication {
 
 	public static void main(String[] args) {
-
+         System.out..println("Subhan");
 	SpringApplication.run(SpringbootJenkinsAppApplication.class, args);
 	}
 
